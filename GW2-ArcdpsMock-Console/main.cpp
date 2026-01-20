1af6fbe09bac899009ff4ffe970534e94be4fcd9
Giacomo Pozzoni <giacomopoz@gmail.com>
2026-01-20 22:38:22 +0100

    Describe how the app will work

--- GW2-ArcdpsMock-Console/main.cpp
+++ GW2-ArcdpsMock-Console/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+    // 1. Load libraries from the specified input folder, multiplatform (Windows and Linux)
+    // 1.a. Initialize each library calling mod_init
+    // 2. Start UI thread. It will call mod_gui on the loaded libraries
+    // 3. Start a loop that will:
+    // 3.a. Ask for input: exit, repeat the previous log (if any was ran) or load and run the specified combat log
+    // 3.b. Execute the requested action
+
+    return 0;
 }
