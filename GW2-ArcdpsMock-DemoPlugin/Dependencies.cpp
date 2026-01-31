afc305fb15c13594b64db4257b3229bb0549e9a1
Giacomo Pozzoni <giacomopoz@gmail.com>
2026-01-31 19:44:05 +0100

    Use crossplatform arcdps-unofficial-extras

--- GW2-ArcdpsMock-DemoPlugin/Dependencies.cpp
+++ GW2-ArcdpsMock-DemoPlugin/Dependencies.cpp
@@ -1,6 +1,7 @@
 #include "Dependencies.h"
 #include <magic_enum/magic_enum.hpp>
 #include <ArcdpsUnofficialExtras/Definitions.h>
+#include <CrossplatformMocks/intsafe.h>
 
 void TriggereDependenciesLoad()
 {
